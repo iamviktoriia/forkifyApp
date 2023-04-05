547687b3b8e0eb2db54465e21087412a35c270ca
iamviktoriia <vikaantoniuk@gmail.com>
2023-04-05 12:41:54 +0300

    implement update method to avoid page rerendering

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,6 +19,9 @@ const controlRecipes = async function () {
         if (!id) return;
         recipeView.renderSpinner();
 
+        //0) Update results view to mark selected search result
+        resultsView.update(model.getSearchResulsPage());
+
         //1) Loading recipe
         await model.loadRecipe(id);
 
@@ -52,7 +55,8 @@ const controlPagination = function (goToPage) {
 const controlServings = function (newServings) {
     model.updateServings(newServings);
 
-    recipeView.render(model.state.recipe);
+    // recipeView.render(model.state.recipe);
+    recipeView.update(model.state.recipe);
 };
 
 
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -13,6 +13,27 @@ export default class View {
 
     }
 
+    update(data) {
+        this._data = data;
+        const newMarkup = this._generateMarkup();
+
+        const newDom = document.createRange().createContextualFragment(newMarkup);
+        const newElements = Array.from(newDom.querySelectorAll('*'));
+        const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+        newElements.forEach((newEl, i) => {
+            const curEl = curElements[i];
+            console.log('newEl.isequalNode', curEl, newEl.isEqualNode(curEl));
+
+            if (!newEl.isEqualNode(curEl) && newEl.firstChild?.nodeValue.trim() !== '') {
+                curEl.textContent = newEl.textContent;
+            }
+            if (!newEl.isEqualNode(curEl)){
+                Array.from(newEl.attributes).forEach(attr => curEl.setAttribute(attr.name, attr.value));
+            }
+        })
+    };
+
     _clear() {
         console.log(this._parentElement);
         this._parentElement.innerHTML = '';
