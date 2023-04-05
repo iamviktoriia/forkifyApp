2e909cd543880c2b5dbba4ecf6b26fd9d9e30611
iamviktoriia <vikaantoniuk@gmail.com>
2023-04-05 12:48:33 +0300

    fixed pagination bug

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2674,6 +2674,7 @@ const loadSearchResults = async function(query) {
                 image: rec.image_url
             };
         });
+        state.search.page = 1;
     } catch (err) {
         console.error(`${err}`);
         throw err;
